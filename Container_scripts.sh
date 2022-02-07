bf0c2e1fb0fd205fbc441b3c07acb61ad02ecbc6
Muppity <caltls@gmail.com>
2022-02-06 21:35:36 -0600

    changing containers

---
+++ Container_scripts.sh
@@ -0,0 +1,13 @@
+#Create DB Image In Docker
+docker build --label 'img_my_tests' -t imgmytests   .
+
+#Create DB Instance
+docker image ls 
+docker run --name=msqlTest1 -d imgmytests 
+docker stop msqlTest1
+docker rm msqlTest1
+docker ps -a
+
+
+#Removing code
+docker image rm 047b -f
