1df622698a754d0969bc84aeba52f49e4f9e0bea
Carlos Lopez <caltls@hotmail.com>
2023-09-07 11:17:33 -0600

    adding Oracle images to the local registry

--- Create_Containers.sh
+++ Create_Containers.sh
@@ -9,7 +9,7 @@ echo 'Create DB Instance Container mysqlTest2 💾';
 docker run --name=mysqlTest2 -d imgmytests
 
 
-docker run -d --name=mysqlTest1   -e 'MYSQL_ROOT_PASSWORD=''Clave01*''' 434c35b82b08\
+docker run -d --name=mysqlTest1   -e 'MYSQL_ROOT_PASSWORD=''Clave01*''' 5a9594052aec\
  -e 'MYSQL_ALLOW_EMPTY_PASSWORD=''No'''\
  -e 'MYSQL_RANDOM_ROOT_PASSWORD=''No'''
 
@@ -29,3 +29,19 @@ docker run -d --name=graf_tests_ent \
   -e "GF_LOG_MODE=console file" \
   -e "GF_INSTALL_PLUGINS=grafana-clock-panel,grafana-simple-json-datasource" \
   img_graf_tests
+
+
+#my oracle
+
+docker run -d --name orc_test \
+  container-registry.oracle.com/database/free:latest
+
+  docker run -d --env-file .ora_db_env.dat -p 1521:1521 --name=orcl_prbs container-registry.oracle.com/database/free:latest
+
+  docker exec -it orcl_prbs bash ./setPassword.sh
+
+  docker exec -it orcl_prbs sqlplus sys/Clave01* as sysdba
+  docker exec -it orcl_prbs sqlplus / as sysdba
+  podman exec -it <oracle-db> sqlplus sys/<your_password>@FREE as sysdba
+  docker exec -it <oracle-db> sqlplus system/<your_password>@FREE
+  docker exec -it <oracle-db> sqlplus pdbadmin/<your_password>@FREEPDB1
